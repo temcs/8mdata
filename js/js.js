68172843b161fdeb2f60c15a305e6ff05631cff6
8Mbets <160002063+8Mbets@users.noreply.github.com>
2024-05-28 16:20:53 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,52 +2,52 @@
 const gameset = {
     // Sunday
     sunday: {
-        "9am-3pm": ["JILI স্লটের Fortune Gems", "PG স্লটের Wild Bounty Showdown", "JILI স্লটের Aztec Priestess", "PG স্লটের Jurassic Kingdom"],
-        "3pm-9pm": ["BNG স্লটের Sun of Egypt 2", "JILI স্লটের Wild Ace", "PG স্লটের Secrets of Cleopatra", "BNG স্লটের Prosperity Fortune Tree"],
-        "9pm-3am": ["PP স্লটের Gates of Olympus™", "PP স্লটের Barn Festival", "JILI স্লটের Mines", "BNG স্লটের Black Wolf"],
-        "3am-9am": ["REVO স্লটের Alchemy Book", "JILI স্লটের Charge Buffalo", "PP স্লটের Aztec Gems Deluxe", "BNG স্লটের Buddha Fortune"]
+      "9am-3pm": ["PG স্লটের Secrets of Cleopatra", "PG স্লটের Wild Bounty Showdown", "PP স্লটের Barn Festival", "PP স্লটের Gates of Olympus™"],
+      "3pm-9pm": ["HAK ক্রাশ খেলা Mines", "JILI স্লটের Fortune Gems", "JILI ফিসিং Royal Fishing", "JILI স্লটের Mines"],
+      "9pm-3am": ["BNG স্লটের Sun of Egypt 2", "PP স্লটের Aztec Gems Deluxe", "JILI স্লটের Dragon Treasure", "JILI স্লটের GoRush"],
+      "3am-9am": ["JILI ফিসিং Dinosaur Tycoon II", "JILI স্লটের Super Ace", "JILI ফিসিং Golden Land", "JILI স্লটের Charge Buffalo"],
       },
     // Monday
     monday: {
-        "9am-3pm": ["PG স্লটের Safari Wilds", "PG স্লটের Secrets of Cleopatra", "PG স্লটের Reel Love", "JILI ফিসিং Bombing Fishing"],
-        "3pm-9pm": ["JILI ফিসিং Golden Land", "JILI ফিসিং Dinosaur Tycoon II", "JILI ফিসিং Royal Fishing", "JILI স্লটের Super Ace"],
-        "9pm-3am": ["PG স্লটের Cruise Royale", "BNG স্লটের Book of Wizard", "JILI স্লটের GoRush", "JILI স্লটের Dragon & Tiger"],
-        "3am-9am": ["HAK ক্রাশ খেলা Mines", "", "", ""]
-    },
+      "9am-3pm": ["PG স্লটের Secrets of Cleopatra", "JILI ফিসিং Dinosaur Tycoon II", "JILI স্লটের Aztec Priestess", "PP স্লটের Aztec Gems Deluxe"],
+      "3pm-9pm": ["HAK ক্রাশ খেলা Mines", "PG স্লটের Reel Love", "JILI স্লটের Charge Buffalo", "BNG স্লটের Book of Wizard"],
+      "9pm-3am": ["PP স্লটের Barn Festival", "BNG স্লটের Prosperity Fortune Tree", "BNG স্লটের Black Wolf", "BNG স্লটের Buddha Fortune"],
+      "3am-9am": ["JILI স্লটের Dragon & Tiger", "JILI স্লটের Fortune Gems", "BNG স্লটের Sun of Egypt 2", "JILI স্লটের Super Ace"],
+      },
     // Tuesday
     tuesday: {
-        "9am-3pm": ["JILI স্লটের Wild Bounty Showdown", "BNG স্লটের Sun of Egypt 2", "JILI স্লটের Wild Ace", "PG স্লটের Secrets of Cleopatra"],
-        "3pm-9pm": ["BNG স্লটের Prosperity Fortune Tree", "PP স্লটের Gates of Olympus™", "PP স্লটের Barn Festival", "JILI স্লটের Mines"],
-        "9pm-3am": ["BNG স্লটের Black Wolf", "REVO স্লটের Alchemy Book", "JILI স্লটের Charge Buffalo", "PP স্লটের Aztec Gems Deluxe"],
-        "3am-9am": ["BNG স্লটের Buddha Fortune", "PG স্লটের Safari Wilds", "PG স্লটের Secrets of Cleopatra", "PG স্লটের Reel Love"]
+      "9am-3pm": ["JILI ফিসিং Bombing Fishing", "PP স্লটের Barn Festival", "JILI স্লটের Wild Ace", "JILI স্লটের Aztec Priestess"],
+      "3pm-9pm": ["JILI স্লটের Dragon Treasure", "BNG স্লটের Sun of Egypt 2", "JILI স্লটের Super Ace", "JILI ফিসিং Royal Fishing"],
+      "9pm-3am": ["HAK ক্রাশ খেলা Mines", "PG স্লটের Safari Wilds", "PG স্লটের Reel Love", "BNG স্লটের Prosperity Fortune Tree"],
+      "3am-9am": ["PG স্লটের Wild Bounty Showdown", "PP স্লটের Aztec Gems Deluxe", "JILI ফিসিং Dinosaur Tycoon II", "JILI স্লটের Charge Buffalo"],
       },
     // Wednesday
     wednesday: {
-        "9am-3pm": ["JILI ফিসিং Bombing Fishing", "JILI ফিসিং Golden Land", "JILI ফিসিং Dinosaur Tycoon II", "JILI ফিসিং Royal Fishing"],
-        "3pm-9pm": ["JILI স্লটের Super Ace", "PG স্লটের Cruise Royale", "BNG স্লটের Book of Wizard", "JILI স্লটের GoRush"],
-        "9pm-3am": ["JILI স্লটের Dragon & Tiger", "HAK ক্রাশ খেলা Mines", "", ""],
-        "3am-9am": ["", "", "", ""]
+      "9am-3pm": ["BNG স্লটের Book of Wizard", "JILI ফিসিং Golden Land", "JILI স্লটের Aztec Priestess", "JILI ফিসিং Bombing Fishing"],
+      "3pm-9pm": ["JILI স্লটের Charge Buffalo", "JILI স্লটের Super Ace", "JILI স্লটের GoRush", "REVO স্লটের Alchemy Book"],
+      "9pm-3am": ["JILI ফিসিং Royal Fishing", "HAK ক্রাশ খেলা Mines", "PG স্লটের Safari Wilds", "PG স্লটের Reel Love"],
+      "3am-9am": ["PG স্লটের Wild Bounty Showdown", "PP স্লটের Gates of Olympus™", "JILI স্লটের Dragon & Tiger", "JILI স্লটের Wild Ace"],
       },
     // Thursday
     thursday: {
-        "9am-3pm": ["BNG স্লটের Sun of Egypt 2", "JILI স্লটের Wild Ace", "PG স্লটের Secrets of Cleopatra", "BNG স্লটের Prosperity Fortune Tree"],
-        "3pm-9pm": ["PP স্লটের Gates of Olympus™", "PP স্লটের Barn Festival", "JILI স্লটের Mines", "BNG স্লটের Black Wolf"],
-        "9pm-3am": ["REVO স্লটের Alchemy Book", "JILI স্লটের Charge Buffalo", "PP স্লটের Aztec Gems Deluxe", "BNG স্লটের Buddha Fortune"],
-        "3am-9am": ["PG স্লটের Safari Wilds", "PG স্লটের Secrets of Cleopatra", "PG স্লটের Reel Love", "JILI ফিসিং Bombing Fishing"]
+      "9am-3pm": ["BNG স্লটের Book of Wizard", "JILI ফিসিং Golden Land", "JILI স্লটের Aztec Priestess", "JILI ফিসিং Bombing Fishing"],
+      "3pm-9pm": ["JILI স্লটের Charge Buffalo", "JILI স্লটের Super Ace", "JILI স্লটের GoRush", "REVO স্লটের Alchemy Book"],
+      "9pm-3am": ["JILI ফিসিং Royal Fishing", "HAK ক্রাশ খেলা Mines", "PG স্লটের Safari Wilds", "PG স্লটের Reel Love"],
+      "3am-9am": ["PG স্লটের Wild Bounty Showdown", "PP স্লটের Gates of Olympus™", "JILI স্লটের Dragon & Tiger", "JILI স্লটের Wild Ace"],
       },
     // Friday
     friday: {
-        "9am-3pm": ["JILI ফিসিং Golden Land", "JILI ফিসিং Dinosaur Tycoon II", "JILI ফিসিং Royal Fishing", "JILI স্লটের Super Ace"],
-        "3pm-9pm": ["PG স্লটের Cruise Royale", "BNG স্লটের Book of Wizard", "JILI স্লটের GoRush", "JILI স্লটের Dragon & Tiger"],
-        "9pm-3am": ["HAK ক্রাশ খেলা Mines", "", "", ""],
-        "3am-9am": ["", "", "", ""]
+      "9am-3pm": ["BNG স্লটের Book of Wizard", "JILI ফিসিং Golden Land", "JILI স্লটের Aztec Priestess", "JILI ফিসিং Bombing Fishing"],
+      "3pm-9pm": ["JILI স্লটের Charge Buffalo", "JILI স্লটের Super Ace", "JILI স্লটের GoRush", "REVO স্লটের Alchemy Book"],
+      "9pm-3am": ["JILI ফিসিং Royal Fishing", "HAK ক্রাশ খেলা Mines", "PG স্লটের Safari Wilds", "PG স্লটের Reel Love"],
+      "3am-9am": ["PG স্লটের Wild Bounty Showdown", "PP স্লটের Gates of Olympus™", "JILI স্লটের Dragon & Tiger", "JILI স্লটের Wild Ace"],
       },
     // Saturday
     saturday: {
-        "9am-3pm": ["JILI ফিসিং Golden Land", "JILI ফিসিং Dinosaur Tycoon II", "JILI ফিসিং Royal Fishing", "JILI স্লটের Super Ace"],
-        "3pm-9pm": ["PG স্লটের Cruise Royale", "BNG স্লটের Book of Wizard", "JILI স্লটের GoRush", "JILI স্লটের Dragon & Tiger"],
-        "9pm-3am": ["HAK ক্রাশ খেলা Mines", "", "", ""],
-        "3am-9am": ["", "", "", ""]
+      "9am-3pm": ["BNG স্লটের Book of Wizard", "JILI ফিসিং Golden Land", "JILI স্লটের Aztec Priestess", "JILI ফিসিং Bombing Fishing"],
+      "3pm-9pm": ["JILI স্লটের Charge Buffalo", "JILI স্লটের Super Ace", "JILI স্লটের GoRush", "REVO স্লটের Alchemy Book"],
+      "9pm-3am": ["JILI ফিসিং Royal Fishing", "HAK ক্রাশ খেলা Mines", "PG স্লটের Safari Wilds", "PG স্লটের Reel Love"],
+      "3am-9am": ["PG স্লটের Wild Bounty Showdown", "PP স্লটের Gates of Olympus™", "JILI স্লটের Dragon & Tiger", "JILI স্লটের Wild Ace"],
       },
   };
   

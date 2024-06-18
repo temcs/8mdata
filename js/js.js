f70e507558e8cd37a88d8159dfab245d98c9e52e
8Mbets <160002063+8Mbets@users.noreply.github.com>
2024-06-18 15:25:40 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,59 +2,66 @@
 const gameset = {
     // Sunday
     sunday: {
-      "9am-3pm": ["71% - PG স্লটের Cruise Royale", "70% - PP স্লটের Gates of Olympus™", "72% - JILI স্লটের Wild Ace", "66% - PP স্লটের Barn Festival"],
-      "3pm-9pm": ["81% - JILI ফিসিং Bombing Fishing", "79% - JILI স্লটের Super Ace", "75% - JILI স্লটের Fortune Gems", "75% - BNG স্লটের Black Wolf"],
-      "9pm-3am": ["68% - JILI স্লটের Dragon & Tiger", "81% - JILI স্লটের Mines", "71% - PG স্লটের Wild Bounty Showdown", "71% - PG স্লটের Reel Love"],
-      "3am-9am": ["71% - BNG স্লটের Prosperity Fortune Tree", "71% - BNG স্লটের Sun of Egypt 2", "82% - JILI ফিসিং Dinosaur Tycoon II", "81% - JILI ফিসিং Golden Land"],
+      "9am-3pm": ["75% - BNG স্লটের Black Wolf", "78% - PG স্লটের Secrets of Cleopatra", "81% - JILI ফিসিং Golden Land", "81% - JILI স্লটের Mines"],
+      "3pm-9pm": ["72% - REVO স্লটের Alchemy Book", "73% - HAK ক্রাশ খেলা Mines", "75% - JILI স্লটের Fortune Gems", "71% - JILI স্লটের Money Coming"],
+      "9pm-3am": ["71% - PG স্লটের Wild Bounty Showdown", "78% - PG স্লটের Candy Burst", "79% - JILI স্লটের Aztec Priestess", "69% - JILI স্লটের Dragon Treasure"],
+      "3am-9am": ["73% - HAK ক্রাশ খেলা Mines", "72% - JILI স্লটের Wild Ace", "81% - JILI ফিসিং Golden Land", "76% - BNG স্লটের Book of Wizard"],
       }
+      
       ,
     // Monday
     monday: {
-      "9am-3pm": ["72% - REVO স্লটের Alchemy Book", "75% - BNG স্লটের Black Wolf", "69% - JILI স্লটের Dragon Treasure", "79% - JILI স্লটের Super Ace"],
-      "3pm-9pm": ["81% - JILI ফিসিং Golden Land", "71% - PG স্লটের Cruise Royale", "71% - BNG স্লটের Sun of Egypt 2", "71% - PG স্লটের Reel Love"],
-      "9pm-3am": ["71% - PG স্লটের Wild Bounty Showdown", "69% - JILI স্লটের Dragon Treasure", "75% - JILI স্লটের Fortune Gems", "63% - HAK ক্রাশ খেলা Mines"],
-      "3am-9am": ["76% - BNG স্লটের Book of Wizard", "81% - JILI স্লটের Mines", "78% - PG স্লটের Secrets of Cleopatra", "72% - JILI স্লটের Wild Ace"],
+      "9am-3pm": ["71% - BNG স্লটের Sun of Egypt 2", "78% - PG স্লটের Secrets of Cleopatra", "82% - JILI ফিসিং Dinosaur Tycoon II", "70% - JILI স্লটের Dragon & Tiger"],
+      "3pm-9pm": ["71% - JILI স্লটের Money Coming", "81% - JILI ফিসিং Bombing Fishing", "79% - JILI স্লটের Aztec Priestess", "75% - JILI স্লটের Fortune Gems"],
+      "9pm-3am": ["71% - BNG স্লটের Prosperity Fortune Tree", "72% - REVO স্লটের Alchemy Book", "69% - JILI স্লটের Dragon Treasure", "76% - PP স্লটের Barn Festival"],
+      "3am-9am": ["78% - BNG স্লটের Buddha Fortune", "71% - PP স্লটের Aztec Gems Deluxe", "70% - PP স্লটের Gates of Olympus™", "79% - JILI স্লটের Super Ace"],
       }
+      
       ,
     // Tuesday
     tuesday: {
-      "9am-3pm": ["79% - JILI স্লটের Aztec Priestess", "82% - JILI ফিসিং Dinosaur Tycoon II", "71% - BNG স্লটের Sun of Egypt 2", "68% - JILI স্লটের Dragon & Tiger"],
-      "3pm-9pm": ["68% - JILI স্লটের GoRush", "81% - JILI স্লটের Mines", "71% - BNG স্লটের Prosperity Fortune Tree", "63% - HAK ক্রাশ খেলা Mines"],
-      "9pm-3am": ["79% - JILI স্লটের Super Ace", "69% - JILI স্লটের Dragon Treasure", "70% - PP স্লটের Gates of Olympus™", "81% - JILI ফিসিং Golden Land"],
-      "3am-9am": ["75% - BNG স্লটের Black Wolf", "68% - BNG স্লটের Buddha Fortune", "72% - JILI স্লটের Wild Ace", "68% - PG স্লটের Safari Wilds"],
+      "9am-3pm": ["76% - PP স্লটের Barn Festival", "71% - PG স্লটের Wild Bounty Showdown", "73% - HAK ক্রাশ খেলা Mines", "79% - JILI স্লটের Super Ace"],
+      "3pm-9pm": ["78% - PG স্লটের Candy Burst", "76% - BNG স্লটের Book of Wizard", "81% - JILI স্লটের Mines", "79% - JILI স্লটের Aztec Priestess"],
+      "9pm-3am": ["71% - BNG স্লটের Sun of Egypt 2", "81% - JILI ফিসিং Bombing Fishing", "71% - JILI স্লটের Money Coming", "78% - PG স্লটের Secrets of Cleopatra"],
+      "3am-9am": ["81% - JILI ফিসিং Golden Land", "75% - JILI স্লটের Fortune Gems", "71% - BNG স্লটের Prosperity Fortune Tree", "71% - PP স্লটের Aztec Gems Deluxe"],
       }
+      
       ,
     // Wednesday
     wednesday: {
-      "9am-3pm": ["66% - PP স্লটের Barn Festival", "69% - JILI স্লটের Dragon Treasure", "79% - JILI স্লটের Aztec Priestess", "70% - PP স্লটের Gates of Olympus™"],
-      "3pm-9pm": ["75% - BNG স্লটের Black Wolf", "71% - BNG স্লটের Sun of Egypt 2", "68% - JILI স্লটের Dragon & Tiger", "81% - JILI ফিসিং Royal Fishing"],
-      "9pm-3am": ["69% - JILI স্লটের Dragon Treasure", "82% - JILI ফিসিং Dinosaur Tycoon II", "71% - PG স্লটের Wild Bounty Showdown", "81% - JILI ফিসিং Golden Land"],
-      "3am-9am": ["71% - BNG স্লটের Prosperity Fortune Tree", "66% - PP স্লটের Barn Festival", "71% - PG স্লটের Cruise Royale", "70% - PP স্লটের Gates of Olympus™"],
+      "9am-3pm": ["76% - BNG স্লটের Book of Wizard", "82% - JILI ফিসিং Dinosaur Tycoon II", "75% - BNG স্লটের Black Wolf", "71% - PP স্লটের Aztec Gems Deluxe"],
+      "3pm-9pm": ["79% - JILI স্লটের Aztec Priestess", "81% - JILI ফিসিং Golden Land", "70% - PP স্লটের Gates of Olympus™", "73% - HAK ক্রাশ খেলা Mines"],
+      "9pm-3am": ["71% - PG স্লটের Wild Bounty Showdown", "76% - PP স্লটের Barn Festival", "78% - BNG স্লটের Buddha Fortune", "78% - PG স্লটের Candy Burst"],
+      "3am-9am": ["71% - BNG স্লটের Prosperity Fortune Tree", "75% - JILI স্লটের Charge Buffalo", "79% - JILI স্লটের Super Ace", "71% - JILI স্লটের Money Coming"],
       }
+      
       ,
     // Thursday
     thursday: {
-      "9am-3pm": ["69% - JILI স্লটের Dragon Treasure", "71% - BNG স্লটের Prosperity Fortune Tree", "71% - BNG স্লটের Sun of Egypt 2", "81% - JILI ফিসিং Bombing Fishing"],
-      "3pm-9pm": ["81% - JILI ফিসিং Golden Land", "75% - JILI স্লটের Charge Buffalo", "81% - JILI স্লটের Mines", "71% - PG স্লটের Wild Bounty Showdown"],
-      "9pm-3am": ["68% - BNG স্লটের Buddha Fortune", "63% - HAK ক্রাশ খেলা Mines", "82% - JILI ফিসিং Dinosaur Tycoon II", "71% - PG স্লটের Cruise Royale"],
-      "3am-9am": ["79% - JILI স্লটের Aztec Priestess", "78% - PG স্লটের Secrets of Cleopatra", "68% - JILI স্লটের GoRush", "66% - PP স্লটের Barn Festival"],
+      "9am-3pm": ["71% - PP স্লটের Aztec Gems Deluxe", "71% - PG স্লটের Symbols of Egypt", "76% - BNG স্লটের Book of Wizard", "81% - JILI ফিসিং Golden Land"],
+      "3pm-9pm": ["79% - JILI স্লটের Super Ace", "78% - PG স্লটের Candy Burst", "78% - PG স্লটের Secrets of Cleopatra", "75% - JILI স্লটের Charge Buffalo"],
+      "9pm-3am": ["69% - JILI স্লটের Dragon Treasure", "81% - JILI ফিসিং Bombing Fishing", "78% - BNG স্লটের Buddha Fortune", "71% - PG স্লটের Reel Love"],
+      "3am-9am": ["76% - BNG স্লটের Book of Wizard", "81% - JILI ফিসিং Golden Land", "79% - JILI স্লটের Super Ace", "71% - PG স্লটের Wild Bounty Showdown"],
       }
+      
       ,
     // Friday
     friday: {
-      "9am-3pm": ["81% - JILI ফিসিং Bombing Fishing", "69% - JILI স্লটের Dragon Treasure", "71% - PP স্লটের Aztec Gems Deluxe", "63% - HAK ক্রাশ খেলা Mines"],
-      "3pm-9pm": ["78% - PG স্লটের Secrets of Cleopatra", "72% - REVO স্লটের Alchemy Book", "75% - JILI স্লটের Fortune Gems", "79% - JILI স্লটের Aztec Priestess"],
-      "9pm-3am": ["81% - JILI ফিসিং Royal Fishing", "66% - PP স্লটের Barn Festival", "76% - BNG স্লটের Book of Wizard", "82% - JILI ফিসিং Dinosaur Tycoon II"],
-      "3am-9am": ["71% - PG স্লটের Reel Love", "78% - PG স্লটের Secrets of Cleopatra", "72% - JILI স্লটের Wild Ace", "68% - BNG স্লটের Buddha Fortune"],
+      "9am-3pm": ["81% - JILI ফিসিং Bombing Fishing", "75% - BNG স্লটের Black Wolf", "71% - PG স্লটের Symbols of Egypt", "79% - JILI স্লটের Super Ace"],
+      "3pm-9pm": ["71% - PG স্লটের Reel Love", "71% - PG স্লটের Wild Bounty Showdown", "75% - JILI স্লটের Charge Buffalo", "70% - JILI স্লটের Dragon & Tiger"],
+      "9pm-3am": ["73% - HAK ক্রাশ খেলা Mines", "71% - BNG স্লটের Sun of Egypt 2", "81% - JILI স্লটের Mines", "75% - JILI স্লটের Fortune Gems"],
+      "3am-9am": ["75% - BNG স্লটের Black Wolf", "71% - JILI স্লটের Money Coming", "71% - PP স্লটের Aztec Gems Deluxe", "69% - JILI স্লটের Dragon Treasure"],
       }
+      
       ,
     // Saturday
     saturday: {
-      "9am-3pm": ["78% - PG স্লটের Secrets of Cleopatra", "71% - PP স্লটের Aztec Gems Deluxe", "68% - JILI স্লটের Dragon & Tiger", "75% - JILI স্লটের Fortune Gems"],
-      "3pm-9pm": ["71% - PG স্লটের Reel Love", "81% - JILI ফিসিং Golden Land", "71% - BNG স্লটের Prosperity Fortune Tree", "75% - JILI স্লটের Charge Buffalo"],
-      "9pm-3am": ["79% - JILI স্লটের Aztec Priestess", "70% - PP স্লটের Gates of Olympus™", "72% - REVO স্লটের Alchemy Book", "66% - PP স্লটের Barn Festival"],
-      "3am-9am": ["69% - JILI স্লটের Dragon Treasure", "82% - JILI ফিসিং Dinosaur Tycoon II", "71% - PG স্লটের Wild Bounty Showdown", "76% - BNG স্লটের Book of Wizard"],
+      "9am-3pm": ["71% - BNG স্লটের Sun of Egypt 2", "72% - REVO স্লটের Alchemy Book", "76% - PP স্লটের Barn Festival", "81% - JILI ফিসিং Bombing Fishing"],
+      "3pm-9pm": ["71% - JILI স্লটের Money Coming", "69% - JILI স্লটের Dragon Treasure", "81% - JILI ফিসিং Royal Fishing", "82% - JILI ফিসিং Dinosaur Tycoon II"],
+      "9pm-3am": ["71% - PG স্লটের Wild Bounty Showdown", "75% - BNG স্লটের Black Wolf", "71% - BNG স্লটের Prosperity Fortune Tree", "81% - JILI স্লটের Mines"],
+      "3am-9am": ["75% - JILI স্লটের Charge Buffalo", "70% - JILI স্লটের Dragon & Tiger", "70% - PP স্লটের Gates of Olympus™", "81% - JILI ফিসিং Golden Land"],
       }
+      
       ,
   };
   
@@ -146,3 +153,34 @@ function copyText() {
         }, 8); // Adjust the interval here for faster counting
     });
 });
+
+
+
+// screenshoot 
+
+    // Ensure html2canvas is loaded before using it
+    window.onload = function() {
+      document.getElementById('screenshotButton').addEventListener('click', function() {
+        // Capture screenshot
+        html2canvas(document.body).then(function(canvas) {
+          // Convert canvas to blob
+          canvas.toBlob(function(blob) {
+            // Create a link element
+            var link = document.createElement('a');
+            link.download = 'screenshot.png'; // Set the default filename
+
+            // Create a URL for the blob
+            var url = URL.createObjectURL(blob);
+            link.href = url;
+
+            // Append the link to the body and trigger a click event
+            document.body.appendChild(link);
+            link.click();
+
+            // Clean up
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+          });
+        });
+      });
+    };
